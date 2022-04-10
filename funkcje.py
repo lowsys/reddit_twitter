6b8b74f0f5efc966a56895d60cf44fda515885e4
lowsys <62142337+lowsys@users.noreply.github.com>
2022-04-10 16:25:08 +0200

    Add files via upload

--- funkcje.py
+++ funkcje.py
@@ -133,6 +133,9 @@ def calculate_propagation_time(file_name):
             df.iloc[row_index, 8] = (int(df.iloc[row_index, 3]) - int(df.iloc[row_index, 7])) / 60
             row_index = row_index + 1
     print(df)
+    df = df.drop(labels=0, axis=0)
+    print(df)
+    print(df['difference_in_minutes'].describe())
 download_reddit_data('2022, 2, 1', '2022, 2, 3', 'polska')
 
 calculate_propagation_time('2022-2-1_2022-2-3_polskalist.csv')
